f04d03d7464972299f0a4dc4e2736029c28e1a1d
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-06-21 00:23:18 -0700

    Trying to find a size that works...

--- html/src/main/java/no/sandramoen/libgdx33/gwt/GwtLauncher.java
+++ html/src/main/java/no/sandramoen/libgdx33/gwt/GwtLauncher.java
@@ -16,7 +16,7 @@ public class GwtLauncher extends GwtApplication {
 //            return cfg;
             // If you want a fixed size application, comment out the above resizable section,
             // and uncomment below:
-            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration();
+            GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(900, 900);
             cfg.padHorizontal = 0;
             cfg.padVertical = 0;
             return cfg;
